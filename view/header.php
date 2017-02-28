45c5fc642b39421ca984868a6fe7bca06dc44820
David Yi <dy89@njit.edu>
2017-02-28 15:41:19 -0500

    change main.css path

--- view/header.php
+++ view/header.php
@@ -4,7 +4,7 @@
 <head>
     <title>My Guitar Shop</title>
     <link rel="stylesheet" type="text/css"
-          href="/is218_5-1/main.css">
+          href="main.css">
 </head>
 
 <!-- the body section -->
